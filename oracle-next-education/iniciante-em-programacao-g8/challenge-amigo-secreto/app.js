6a6e940373b630897153c785dd8e5806eea8f16d
Saulo <rb.saulodev@gmail.com>
2025-01-22 17:28:02 -0300

    feat: Implementada a função de adicionar amigo
    Função para adicionar amigos ao desafio Amigo Secreto criada com sucesso. Inclui validações básicas e atualização da lista de participantes.

--- oracle-next-education/iniciante-em-programacao-g8/challenge-amigo-secreto/app.js
+++ oracle-next-education/iniciante-em-programacao-g8/challenge-amigo-secreto/app.js
@@ -1,1 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomes = ["Saulo", "Maria"];
+
+function adicionarAmigo(){
+    let nome = document.querySelector("input").value;
+    if(nome == ''){
+        alert("Por favor, digite um nome!");
+    }else{
+        nomes.push(nome);
+        console.log(nomes);
+    }
+}
+
+function sortearAmigo(){
+
+}
