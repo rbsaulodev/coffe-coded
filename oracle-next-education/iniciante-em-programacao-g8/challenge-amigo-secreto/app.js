fa6a62792671fecb3342f091d507d5e6c2bad36b
Saulo <rb.saulodev@gmail.com>
2025-01-23 16:05:18 -0300

    refactor: Melhorias nas funções de adicionar e sortear amigos
    - Criada a função `atualizarMensagem` para centralizar a lógica de atualização de mensagens e cores no campo de resultado.
    - Adicionadas validações à função `adicionarAmigo`:
    - Melhorias gerais na exibição de mensagens e na usabilidade.
    - Limpeza do campo de entrada após adicionar um amigo com sucesso.

--- oracle-next-education/iniciante-em-programacao-g8/challenge-amigo-secreto/app.js
+++ oracle-next-education/iniciante-em-programacao-g8/challenge-amigo-secreto/app.js
@@ -1,38 +1,41 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 let nomes = [];
 
-function inserirTexto(id, texto) {
-    let campo = document.getElementById(id);
-    campo.innerHTML = texto;
+let campoResultado = document.getElementById("resultado");
+
+function atualizarMensagem(mensagem, cor){
+    campoResultado.innerHTML = mensagem;
+    campoResultado.style.color = cor;
 }
 
 function adicionarAmigo(){
-    let campoResultado = document.getElementById("resultado");
     let nome = document.querySelector("input").value;
 
     if(nome == ''){
-        inserirTexto('resultado', "Por favor, digite um nome!");
-        resultado.style.color = "red";
-    }else{
+        atualizarMensagem("Por favor, digite o nome do seu amigo!", "red");
+    }
+    else if(nomes.includes(nome)){
+        atualizarMensagem("Por favor, digite um nome diferente do outro!", "red");
+    }
+    else if(nome.length > 50){
+        atualizarMensagem("O nome é muito longo! Use no máximo 50 caracteres.", "red");
+    }
+    else{
         nomes.push(nome);
-        campoResultado.style.color = "black";
-        console.log(nomes)
+        atualizarMensagem("Amigo adicionado com sucesso", "black");
+        document.querySelector("input").value = "";
     }
 }
 
 function sortearAmigo(){
-    let campoResultado = document.getElementById("resultado");
     if(nomes.length == 0){
-        inserirTexto('resultado', "Por favor, digite os nomes dos participantes!");
-        resultado.style.color = "black";
-    }else if (nomes.length == 1){
-        inserirTexto('resultado', "Por favor, digite mais de um nome!");
-        resultado.style.color = "black";
-    }else{
+        atualizarMensagem("Por favor, digite os nomes dos amigos", "black");
+    }
+    else if (nomes.length == 1){
+        atualizarMensagem("Por favor, digite mais de um amigo", "black");
+    }
+    else{
         let indice = Math.floor(Math.random() * nomes.length);
         let resultado = nomes[indice];
-        inserirTexto('resultado', `Parabéns, o amigo sorteado foi ${resultado}`);
-        campoResultado.style.color = "green";
+        atualizarMensagem(`O amigo sorteado foi o: ${resultado}`, "green");
     }
 }
